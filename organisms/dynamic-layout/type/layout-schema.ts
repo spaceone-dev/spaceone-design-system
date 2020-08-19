4a8d136f8b17a36ecc0e611b8eb9c7862013619e
WANJIN <wanjin@megazone.com>
2020-08-19 19:26:52 +0900

    Added layout schema types

---
+++ organisms/dynamic-layout/type/layout-schema.ts
@@ -0,0 +1,51 @@
+/* eslint-disable camelcase */
+import { DynamicField } from '@/components/organisms/dynamic-field/type';
+
+/** Metadata schema types for Dynamic layout */
+export type DynamicLayoutType = 'item'|'simple-table'|'table'|'query-search-table'
+    |'raw'|'markdown'|'list';
+
+export interface CommonOptions {
+    root_path?: string;
+}
+
+export interface ItemOptions extends CommonOptions {
+    fields: DynamicField[];
+}
+
+export interface SimpleTableOptions extends CommonOptions {
+    fields: DynamicField[];
+}
+
+export interface TableOptions extends CommonOptions {
+    fields: DynamicField[];
+}
+
+export interface QuerySearchTableOptions extends CommonOptions {
+    fields: DynamicField[];
+}
+
+export type RawOptions = CommonOptions
+
+export interface MarkdownOptions extends CommonOptions {
+    markdown: string|{
+        en: string;
+        ko: string;
+    };
+}
+
+export interface ListOptions extends CommonOptions {
+    layouts: DynamicLayout[];
+}
+
+export type DynamicLayoutOptions =
+    ItemOptions | SimpleTableOptions | TableOptions |
+    QuerySearchTableOptions | RawOptions | MarkdownOptions |
+    ListOptions
+
+
+export interface DynamicLayout {
+    name: string;
+    type: DynamicLayoutType;
+    options?: DynamicLayoutOptions;
+}
