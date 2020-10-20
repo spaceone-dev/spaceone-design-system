8eea6723e9f669876072df7ef5301c7be62b0d7c
siyeon <sylee@megazone.com>
2020-10-20 19:44:28 +0900

    Update: text editor component storybook

--- molecules/text-editor/text-editor/PTextEditor.stories.js
+++ molecules/text-editor/text-editor/PTextEditor.stories.js
@@ -1,4 +1,5 @@
 import PTextEditor from '@/components/molecules/text-editor/text-editor/PTextEditor.vue';
+import { computed } from '@vue/composition-api';
 
 export default {
     title: 'molecules/text-editor/text-editor',
@@ -38,82 +39,6 @@ const value = {
             core: 2,
             memory: 0,
         },
-        compute: {
-            security_group_rules: [
-                {
-                    security_group_name: 'spaceone-dev-eks-admin-access-sg',
-                    port_range_min: 22,
-                    port_range_max: 22,
-                    security_group_id: 'sg-086347e970cb3bd7b',
-                    remote_cidr: '54.180.176.16/32',
-                    direction: 'inbound',
-                    protocol: 'tcp',
-                    remote_group_id: null,
-                },
-                {
-                    security_group_id: 'sg-086347e970cb3bd7b',
-                    remote_cidr: '106.244.127.8/32',
-                    direction: 'inbound',
-                    protocol: 'tcp',
-                    remote_group_id: null,
-                    security_group_name: 'spaceone-dev-eks-admin-access-sg',
-                    port_range_min: 22,
-                    port_range_max: 22,
-                },
-                {
-                    security_group_name: 'spaceone-dev-eks-admin-access-sg',
-                    port_range_min: 22,
-                    port_range_max: 22,
-                    security_group_id: 'sg-086347e970cb3bd7b',
-                    remote_cidr: '221.148.35.250/32',
-                    direction: 'inbound',
-                    protocol: 'tcp',
-                    remote_group_id: null,
-                },
-                {
-                    remote_group_id: null,
-                    security_group_name: 'spaceone-dev-eks-admin-access-sg',
-                    port_range_min: 22,
-                    port_range_max: 22,
-                    security_group_id: 'sg-086347e970cb3bd7b',
-                    remote_cidr: '172.16.1.238/32',
-                    direction: 'inbound',
-                    protocol: 'tcp',
-                },
-                {
-                    protocol: 'tcp',
-                    remote_group_id: null,
-                    security_group_name: 'spaceone-dev-eks-admin-access-sg',
-                    port_range_min: 3000,
-                    port_range_max: 3000,
-                    security_group_id: 'sg-086347e970cb3bd7b',
-                    remote_cidr: '221.144.192.28/32',
-                    direction: 'inbound',
-                },
-                {
-                    protocol: 'all',
-                    remote_group_id: null,
-                    security_group_name: 'spaceone-dev-eks-admin-access-sg',
-                    port_range_min: 'all',
-                    port_range_max: 'all',
-                    security_group_id: 'sg-086347e970cb3bd7b',
-                    remote_cidr: '0.0.0.0/0',
-                    direction: 'outbound',
-                },
-            ],
-            instance_type: 'unknown',
-            image: 'ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-20190212.1',
-            created_by_user_id: '072548720675',
-            instance_id: 'i-0ec8d5289d9662156',
-            keypair: 'spaceone-dev-kubectl-ec2-key',
-            instance_name: 'spaceone-dev-eks-cluster_kubectl',
-            static_nat: [
-                '13.125.167.139',
-            ],
-            security_groups: [
-                'spaceone-dev-eks-admin-access-sg',
-            ],
-        },
     },
     nics: [
         {
@@ -131,86 +56,60 @@ const value = {
             tags: {},
         },
     ],
-    disks: [
-        {
-            device_index: 0,
-            device: 'sda1',
-            size: 8,
-            disk_type: 'Ebs',
-            disk_id: '',
-            volume_id: 'vol-0ec7583dc70af963f',
-            storage_id: '',
-            tags: {},
-        },
-    ],
-    template_data: {},
-    pool_info: {
-        pool_id: 'pool-04876e16da9d',
-        name: 'KR01-DEV-spaceone',
-        state: 'ACTIVE',
-        tags: {},
-        domain_id: '',
-        region_info: null,
-        zone_info: null,
-        created_at: null,
-        deleted_at: null,
-    },
-    zone_info: {
-        zone_id: 'zone-eec238fdf433',
-        name: 'KR01-DEV',
-        state: 'ACTIVE',
-        tags: {},
-        region_info: null,
-        domain_id: '',
-        created_at: null,
-        deleted_at: null,
-    },
-    region_info: {
-        region_id: 'region-dc166fcd5836',
-        state: 'ACTIVE',
-        name: 'KR01',
-        tags: {},
-        domain_id: '',
-        created_at: null,
-        deleted_at: null,
-    },
-    project_id: 'project-4b0a522442fc',
-    domain_id: 'domain-22f45f0c5bc4',
-    tags: {},
-    collection_info: {
-        state: 'NEW',
-        collectors: [
-            'collector-d73e5abf65fd',
-            'collector-719e962f838f',
-            'collector-6d004ff04d8a',
-            'collector-5eb1a46c4946',
-        ],
-        update_history: [],
-        pinned_keys: [],
-    },
-    created_at: {
-        seconds: '1568965462',
-        nanos: 956000000,
-    },
-    updated_at: {
-        seconds: '1569384635',
-        nanos: 18000000,
-    },
-    deleted_at: null,
 };
-const data = {
-    code: JSON.stringify(value, undefined, 4),
-};
+
 export const defaultCase = () => ({
     components: { PTextEditor },
+    props: {
+        item: {
+            type: [Object, Array],
+            default: value,
+        },
+        raw: {
+            type: String,
+            default: undefined,
+        },
+        loading: {
+            type: Boolean,
+            default: false,
+        },
+    },
     template: `
-<div style="width: 80vw; height:80vh" class="flex flex-wrap">
-    <PTextEditor :value.sync="code" class="sm:w-1/2 pr-4 pl-4"/>
-    <pre class="sm:w-1/2 pr-4 pl-4">{{code}}</pre>
-</div>`,
-    data() {
+            <div style="width: 80vw; height:80vh" class="flex flex-wrap">
+                <PTextEditor :code.sync="code" class="sm:w-1/2 pr-4 pl-4"/>
+            </div>`,
+    setup(props) {
+        const code = computed(() => JSON.stringify(props.item, undefined, 4));
         return {
-            ...data,
+            code,
+        };
+    },
+});
+
+export const FoldingCase = () => ({
+    components: { PTextEditor },
+    props: {
+        item: {
+            type: [Object, Array],
+            default: value,
+        },
+        raw: {
+            type: String,
+            default: undefined,
+        },
+        loading: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    template: `
+            <div style="width: 80vw; height:80vh" class="flex flex-wrap">
+                <PTextEditor :code.sync="code" class="sm:w-1/2 pr-4 pl-4" :mode="'readOnly'"/>
+            </div>`,
+    setup(props) {
+        const code = computed(() => JSON.stringify(props.item, undefined, 4));
+        return {
+            code,
         };
     },
 });
